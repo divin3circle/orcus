dfe5d113d7909d1a52d5829407ada066ce2bbe86
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-11 22:05:44 +0300

    feat: implement shop handler and store with PostgreSQL integration

--- backend/migrations/00002_shops.sql
+++ backend/migrations/00002_shops.sql
@@ -1,10 +1,10 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE IF NOT EXISTS stores (
+CREATE TABLE IF NOT EXISTS shop (
     id BIGSERIAL PRIMARY KEY,
     -- merchant id
     name VARCHAR(50) UNIQUE NOT NULL,
-    payment_id VARCHAR(10),
+    payment_id VARCHAR(100),
     profile_image_url VARCHAR(2048),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
