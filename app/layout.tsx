b7cfbe6e58933114a0637cedf6bccbbdcaa56b85
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-20 03:12:42 +0300

    feat: add new dependencies for UI components and update global styles with brand colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Montserrat_Alternates } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -10,6 +10,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+});
+
+const montserratAlternates = Montserrat_Alternates({
+  variable: "--font-montserrat-alternates",
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${montserratAlternates.variable} font-montserrat-alternates bg-[#d9d9d9] antialiased`}
       >
         {children}
       </body>
---
+++ components/landing/Navbar.tsx
@@ -0,0 +1,68 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import logo from "@/public/dark-logo.png"
+import Link from 'next/link'
+import { Button } from '../ui/button'
+import { Menu } from 'lucide-react'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+  } from "@/components/ui/drawer"
+import { useCustomerStore } from '@/lib/store'
+
+function Navbar() {
+    const { customer, toggleCustomer } = useCustomerStore()
+  return (
+    <nav className="w-full h-16 bg-foreground mt-2 rounded-3xl flex items-center justify-between">
+      <div className='flex items-center gap-1 px-2'>
+        <Image src={logo} alt="logo" width={42} height={42} />
+        <p className="text-2xl text-background font-semibold">Orcus</p>
+      </div>
+      <div className='px-2'>
+        <div className='md:flex items-center justify-between md:gap-4 xl:gap-8 hidden'>
+            <Link href="/" onClick={() => toggleCustomer("merchant")} className={`text-background uppercase font-semibold text-sm hover:text-background/70 transition-all duration-300 rounded-2xl ${customer === "merchant" ? "border-[1px] border-border/50 px-2 py-1 rounded-3xl" : ""}`}>Merchants</Link>
+            <Link href="/" onClick={() => toggleCustomer("customer")} className={`text-background uppercase font-semibold text-sm hover:text-background/70 transition-all duration-300 rounded-2xl ${customer === "customer" ? "border-[1px] border-border/50 px-2 py-1 rounded-3xl" : ""}`}>Customers</Link>
+            <Link href="/" className='text-background uppercase font-semibold text-sm hover:text-background/70 transition-all duration-300'>Resources</Link>
+            <Button variant="ghost" className='text-[#D7FC6E] font-bold uppercase transition-all duration-300'>Get Started</Button>
+        </div>
+        <Drawer>
+            <DrawerTrigger className='md:hidden'>
+            <Menu className='text-background size-8 md:hidden' /> 
+            </DrawerTrigger>
+            <DrawerContent className='md:hidden'>
+                <DrawerHeader>
+                    <DrawerTitle>
+                    <div className='flex items-center justify-center mb-8 gap-1 px-2'>
+                         <Image src={logo} alt="logo" width={42} height={42} />
+                         <p className="text-2xl font-semibold">Orcus</p>
+                    </div>
+                    </DrawerTitle>
+                    <DrawerDescription>
+                    <div className='flex flex-col gap-8 mb-16'>
+                    <Link href="/" onClick={() => toggleCustomer("merchant")} className={`uppercase font-semibold text-sm hover:text-background/70 transition-all duration-300 rounded-2xl ${customer === "merchant" ? "border-[1px] border-border/50 px-2 py-1 rounded-3xl" : ""}`}>Merchants</Link>
+                    <Link href="/" onClick={() => toggleCustomer("customer")} className={`uppercase font-semibold text-sm hover:text-background/70 transition-all duration-300 rounded-2xl ${customer === "customer" ? "border-[1px] border-border/50 px-2 py-1 rounded-3xl" : ""}`}>Customers</Link>
+                    <Link href="/" className='uppercase font-semibold text-sm hover:text-background/70 transition-all duration-300'>Resources</Link>
+                    <Button variant="ghost" className='text-[#D7FC6E] bg-foreground font-bold uppercase transition-all duration-300'>Get Started</Button>
+                    </div>
+                    </DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                    <DrawerClose className=''>
+                        <Button variant="outline">Cancel</Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
