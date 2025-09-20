163e94f1885c433a315081b7bd2f72d3824dc99d
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-09-20 16:33:17 +0300

    fix: update login form to use username instead of email for authentication and adjust placeholder text

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -9,21 +9,21 @@ export function LoginForm({
 }: React.ComponentProps<"form">) {
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
+      <div className="flex flex-col items-center gap-2 text-center w-full">
         <h1 className="text-2xl font-bold">Welcome Back</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          Enter your username below to login to your account
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
+          <Label htmlFor="email">Username</Label>
           <Input
             id="email"
-            type="email"
-            placeholder="m@example.com"
+            type="username"
+            placeholder="sylus abel"
             required
-            className="border-foreground/30 border-[1px]"
+            className="border-foreground/30 border-[1px] placeholder:text-foreground/50"
           />
         </div>
         <div className="grid gap-3">
